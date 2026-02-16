ef1e4fba4c94209f68be28d7a7469a3e7432921a
Mohamed Fawas <Mohammedfawasfareed@gmail.com>
2026-02-16 14:12:36 +0530

    Clear API key and base image URL in constants
    Removed sensitive API key and base image URL.

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,5 +1,5 @@
 class AppConstants{
-  static const String apiKey = "6eeb53dcb334669c3f7d0945df844cd0";
+  static const String apiKey = "";
   static const String baseUrl = "https://api.themoviedb.org/3/";
-  static const String baseImageUrl = "https://image.tmdb.org/t/p/w500";
+  static const String baseImageUrl = "";
 }
