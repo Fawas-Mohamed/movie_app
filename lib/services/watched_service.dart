af81ee32679ae1266b9e4e4ec610deeca711ee51
Mohamed Fawas <Mohammedfawasfareed@gmail.com>
2026-03-16 13:27:10 +0530

    create-services

--- lib/services/watched_service.dart
+++ lib/services/watched_service.dart
@@ -1,7 +1,42 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:movieapp/models/moviemodel.dart';
 
 class WatchedService {
   static final user = FirebaseAuth.instance.currentUser;
+
+  static CollectionReference get watchedRef {
+    if(user == null){
+      throw Exception("User not logged in");
+    }
+    return FirebaseFirestore.instance
+      .collection('users')
+      .doc(user?.uid)
+      .collection('watched');
+  }
+  static Future<void> saveWatched(MovieModel movie) async{
+    await watchedRef.doc(movie.id.toString()).set({
+      "adult": movie.adult,
+      "backdrop_path": movie.backdropPath,
+      "genre_ids": movie.genreIds,
+      "id": movie.id,
+      "original_language": movie.originalLanguage,
+      "original_title": movie.originalTitle,
+      "overview": movie.overview,
+      "popularity": movie.popularity,
+      "poster_path": movie.posterPath,
+      "release_date": movie.releaseDate.toIso8601String(),
+      "title": movie.title,
+      "video": movie.video,
+      "vote_average": movie.voteAverage,
+      "vote_count": movie.voteCount,
+    });
+  }
+  
+  static Future<void> deleteWatched(String movieId) async{
+    await watchedRef.doc(movieId).delete();
+  }
+  static Stream<int> watchedCount(){
+    return watchedRef.snapshots().map((snapshot)=>snapshot.docs.length);
+  }
 }
--- lib/services/watchlist_service.dart
+++ lib/services/watchlist_service.dart
@@ -1,8 +1,41 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:movieapp/models/moviemodel.dart';
 
 class WatchlistService {
   static final user = FirebaseAuth.instance.currentUser;
-  
+
+  static CollectionReference get WatchlistRef{
+    if(user == null){
+      throw Exception("User not logged in");
+    }
+    return FirebaseFirestore.instance
+    .collection('users')
+    .doc(user?.uid)
+    .collection('watchlist');
+  }
+  static Future<void> saveWatchlist(MovieModel movie) async{
+    await WatchlistRef.doc(movie.id.toString()).set({
+      "adult": movie.adult,
+      "backdrop_path": movie.backdropPath,
+      "genre_ids": movie.genreIds,
+      "id": movie.id,
+      "original_language": movie.originalLanguage,
+      "original_title": movie.originalTitle,
+      "overview": movie.overview,
+      "popularity": movie.popularity,
+      "poster_path": movie.posterPath,
+      "release_date": movie.releaseDate.toIso8601String(),
+      "title": movie.title,
+      "video": movie.video,
+      "vote_average": movie.voteAverage,
+      "vote_count": movie.voteCount,
+    });
+  }
+  static Future<void> deleteWachlist(String movieId) async{
+    await WatchlistRef.doc(movieId).delete();
+  }
+  static Stream<int> watchlistCount(){
+    return WatchlistRef.snapshots().map((snapshot)=>snapshot.docs.length);
+  }
 }
