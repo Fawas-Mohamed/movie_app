44c214d280ab0942817b37c6d8a0ea6dd00a7ff8
Mohamed Fawas <Mohammedfawasfareed@gmail.com>
2026-04-01 17:27:07 +0530

    Remove sensitive API key from constants
    Cleared the API key to prevent exposure.

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -2,7 +2,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class AppConstants{
-  static const String apiKey = "6eeb53dcb334669c3f7d0945df844cd0";
+  static const String apiKey = " ";
   static const String baseUrl = "https://api.themoviedb.org/3/";
   static const String baseImageUrl = "https://image.tmdb.org/t/p/w500/";
 }
